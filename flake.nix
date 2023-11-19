06a77c5f4018157fda446a4279e4466ecc3d5d77
Nikita Pedorich <pedorich-n@users.noreply.github.com>
2023-11-19 06:01:16 +0100

    Better mrpack (#15)
    * Bring back packwiz-modrinth.nix; Actually works now
    
    * Update github action to use nix for mrpack

--- flake.nix
+++ flake.nix
@@ -21,9 +21,14 @@
         in
         {
           packwiz-server = packwiz2nixLib.fetchPackwizModpack {
-            manifest = "${self}/pack.toml";
+            manifest = ./pack.toml;
             hash = "sha256-oJ66+Mxgbtjqzf44QLd5JSuecbwqEeYR1Irx9Oh2RV8=";
             side = "server";
+          };
+
+          modrinth-pack = pkgs.callPackage ./nix/packwiz-modrinth.nix { } {
+            src = ./.;
+            hash = "sha256-NGZeQrrmDSoMmVFAUC5pduRxQaCLd7WwVV6S67tJTwY=";
           };
 
           # Not used for anything right now
---
+++ nix/packwiz-modrinth.nix
@@ -0,0 +1,48 @@
+{ lib, stdenvNoCC, packwiz, jq, zip, unzip, moreutils }: { src, hash, ... } @ args:
+stdenvNoCC.mkDerivation (finalAttrs:
+let
+  resultName = "${finalAttrs.passthru.manifest.name}-v${finalAttrs.passthru.manifest.version}.mrpack";
+in
+{
+  pname = "${finalAttrs.passthru.manifest.name}_Modrinth";
+  version = finalAttrs.passthru.manifest.version;
+
+  outputHashMode = "recursive";
+  outputHashAlgo = "sha256";
+  outputHash = hash;
+
+  dontFixup = true;
+
+  buildInputs = [ packwiz jq unzip zip moreutils ];
+
+  buildPhase = ''
+    packwiz modrinth export --output ./${resultName}
+  '';
+
+  # modrinth.index.json file is not sorted, so non-deterministic :(
+  # see https://github.com/packwiz/packwiz/issues/244
+  installPhase = ''
+    runHook preInstall 
+
+    INDEX_FILE="modrinth.index.json"
+    ZIP_FOLDER="$TMPDIR/unpack"
+    JSON_FILE="$ZIP_FOLDER/$INDEX_FILE"
+
+    unzip -q "./${resultName}" "$INDEX_FILE" -d "$ZIP_FOLDER"
+    
+    jq -S '.files|=sort_by(.path)' "$JSON_FILE" | sponge "$JSON_FILE"
+    touch -acmt 197001010000.00 "$JSON_FILE"
+
+    cd "$ZIP_FOLDER"
+    zip -uXq "../${resultName}" modrinth.index.json
+
+    mkdir -p $out
+    mv ../${resultName} $out/
+
+    runHook postInstall
+  '';
+
+  passthru = {
+    manifest = lib.importTOML "${src}/pack.toml";
+  };
+} // args)
