8f2e74ca778571c93b93b277d070b52ca7750096
Nikita Pedorich <pedorich.n@gmail.com>
2025-03-03 22:08:44 +0900

    Add pack

--- flake.nix
+++ flake.nix
@@ -10,9 +10,14 @@
       url = "github:getchoo/packwiz2nix/rewrite";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    modpack = {
+      url = "gitlab:pablo_peraza/modpack-londoism";
+      flake = false;
+    };
   };
 
-  outputs = inputs@{ flake-parts, systems, packwiz2nix, self, ... }: flake-parts.lib.mkFlake { inherit inputs; } ({ moduleWithSystem, ... }: {
+  outputs = inputs@{ flake-parts, systems, packwiz2nix, self, modpack, ... }: flake-parts.lib.mkFlake { inherit inputs; } ({ moduleWithSystem, ... }: {
     systems = import systems;
 
     perSystem = { system, pkgs, config, lib, ... }: {
@@ -22,15 +27,16 @@
         in
         {
           packwiz-server = packwiz2nixLib.fetchPackwizModpack {
-            manifest = "${self}/pack.toml";
-            hash = "sha256-JDdkCLeIdnHpTbEUA8WkH6G/0flbXBkHrYF9N/AlG8k=";
+            manifest = "${modpack}/pack.toml";
+            hash = "sha256-0b7JQa3L/rGKjl+Mn0w11zWz34McpUn2bz+1WsDfWYQ=";
             side = "server";
           };
 
-          modrinth-pack = pkgs.callPackage ./nix/packwiz-modrinth.nix {
-            src = self;
-            hash = "sha256-qPfIgqP6Xv4tt2p8xnDsspW540Q2We6nWUGYiJynyvM=";
-          };
+          # Not used for anything right now
+          # modrinth-pack = pkgs.callPackage ./nix/packwiz-modrinth.nix {
+          #   src = self;
+          #   hash = "sha256-qPfIgqP6Xv4tt2p8xnDsspW540Q2We6nWUGYiJynyvM=";
+          # };
 
           # Not used for anything right now
           # packwiz-client = packwiz2nixLib.fetchPackwizModpack {
